4bf09f9a000d5368766ccb89590fb748e2400b78
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-02-15 15:44:16 +0100

    sketch for code to update rows

--- Source Stalker/ServerListForm.cs
+++ Source Stalker/ServerListForm.cs
@@ -29,6 +29,37 @@ namespace Source_Stalker {
         }
 
         private void buildGrid() {
+            serverGrid.CellEndEdit += ServerGrid_CellEndEdit;
+            foreach(var server in servers) {
+                var row = new DataGridViewRow();
+                SetRowForServer(server, row);
+                serverGrid.Rows.Add(row);
+            }
+        }
+
+        private void ServerGrid_CellEndEdit(object sender, DataGridViewCellEventArgs e) {
+            var row = serverGrid.Rows.SharedRow(e.RowIndex);
+            SetRowForServer(row);
+        }
+
+        private void SetRowForServer(DataGridViewRow row) {
+            ServerStatus server = serverForRow(row);
+            SetRowForServer(server, row);
+        }
+
+        private ServerStatus serverForRow(DataGridViewRow row) {
+            throw new NotImplementedException();
+        }
+
+        private static void SetRowForServer(ServerStatus server, DataGridViewRow row) {
+            if(server.state == ServerStatus.State.TIME_OUT) {
+                row.SetValues(server.Address, "", "0 (0)/0", $"-{Settings.Default.Timeout}");
+            } else if(server.info !=null) {
+                string playerCountString = $"{server.info.PlayerCount} ({server.info.BotCount})/{server.info.MaxPlayerCount}";
+                row.SetValues(server.Address, server.info.Map, playerCountString, server.PingTime);
+            } else {
+                row.SetValues(server.Address, "", "0 (0)/0", "N/a");
+            }
         }
 
         private void SettingChanging(object sender, System.Configuration.SettingChangingEventArgs e) {
