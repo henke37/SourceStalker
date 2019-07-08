183d682ea03d42f73660db1d4537a36e08d05294
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2019-07-08 22:53:38 +0200

    Move the query and response code to a separate file.

---
+++ QueryLib/Queries.cs
@@ -0,0 +1,233 @@
+﻿using Henke37.IOUtils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Henke37.Valve.Source.ServerQuery {
+
+	internal class A2S_INFO_Request : BaseQuery {
+
+		private const byte Header = 0x54;
+		private const string Payload = "Source Engine Query";
+
+		public override void BuildQuery(Stream s) {
+			using(BinaryWriter w = new BinaryWriter(s, Encoding.UTF8)) {
+				w.Write(Header);
+				w.Write(Payload.ToArray());
+				w.Write((byte)0);
+			}
+		}
+	}
+
+	public class A2S_INFO_Response : BaseResponse {
+		public byte Protocol;
+		public string ServerName;
+		public string Map;
+		public string Folder;
+		public string Game;
+		public short Id;
+		public byte PlayerCount;
+		public byte MaxPlayerCount;
+		public byte BotCount;
+		public ServerTypeEnum ServerType;
+		public Platform Environment;
+		public bool PasswordRequired;
+		public bool VACEnabled;
+		public string GameVersion;
+		public ushort PortNumber;
+		public ushort STVPortNumber;
+		public string STVHostName;
+		public string Tags;
+		public ulong ServerSteamId;
+
+		private const byte HasPortNumber = 0x80;
+		private const byte HasSteamId = 0x10;
+		private const byte HasSTV = 0x40;
+		private const byte HasTags = 0x20;
+		private const byte HasLongGameId = 0x01;
+
+		public byte FreePlayerSlots { get => (byte)(MaxPlayerCount - PlayerCount); }
+		public byte RealPlayers { get => (byte)(PlayerCount - BotCount); }
+
+		public A2S_INFO_Response(Stream s) {
+			Read(s);
+		}
+		public A2S_INFO_Response(BinaryReader r) {
+			Read(r);
+		}
+
+		internal void Read(Stream s) {
+			using(BinaryReader r = new BinaryReader(s, Encoding.UTF8)) {
+				Read(r);
+			}
+		}
+
+		internal void Read(BinaryReader r) {
+			Protocol = r.ReadByte();
+			ServerName = r.ReadNullTerminatedUTF8String();
+			Map = r.ReadNullTerminatedUTF8String();
+			Folder = r.ReadNullTerminatedUTF8String();
+			Game = r.ReadNullTerminatedUTF8String();
+			Id = r.ReadInt16();
+			PlayerCount = r.ReadByte();
+			MaxPlayerCount = r.ReadByte();
+			BotCount = r.ReadByte();
+			ServerType = ParseServerType(r.ReadByte());
+			Environment = ParseEnvironment(r.ReadByte());
+			PasswordRequired = r.ReadBoolean();
+			VACEnabled = r.ReadBoolean();
+			GameVersion = r.ReadNullTerminatedUTF8String();
+			byte EDF = r.ReadByte();
+			if((EDF & HasPortNumber) == HasPortNumber) {
+				PortNumber = r.ReadUInt16();
+			}
+			if((EDF & HasSteamId) == HasSteamId) {
+				ServerSteamId = r.ReadUInt64();
+			}
+			if((EDF & HasSTV) == HasSTV) {
+				STVPortNumber = r.ReadUInt16();
+				STVHostName = r.ReadNullTerminatedUTF8String();
+			}
+			if((EDF & HasTags) == HasTags) {
+				Tags = r.ReadNullTerminatedUTF8String();
+			}
+		}
+
+		private static ServerTypeEnum ParseServerType(byte v) {
+			switch((char)v) {
+				case 'd': return ServerTypeEnum.Dedicated;
+				case 'l': return ServerTypeEnum.Local;
+				case 'p': return ServerTypeEnum.Proxy;
+				default: throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		private Platform ParseEnvironment(byte v) {
+			switch((char)v) {
+				case 'l': return Platform.Linux;
+				case 'w': return Platform.Windows;
+				case 'm':
+				case 'o': return Platform.Mac;
+				default: throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		public enum ServerTypeEnum {
+			Dedicated = 'd',
+			Local = 'l',
+			Proxy = 'p'
+		}
+
+		public enum Platform {
+			Linux = 'l',
+			Windows = 'w',
+			Mac = 'm'
+		}
+	}
+
+	internal class BaseChallengeQuery : BaseQuery {
+		private byte Header;
+		public uint Challenge;
+
+		public BaseChallengeQuery(byte Header) {
+			this.Header = Header;
+			Challenge = 0xFFFFFFFF;
+		}
+
+		public BaseChallengeQuery(byte Header, uint challenge) {
+			this.Header = Header;
+			Challenge = challenge;
+		}
+
+		public override void BuildQuery(Stream s) {
+			using(BinaryWriter w = new BinaryWriter(s, Encoding.UTF8)) {
+				w.Write(Header);
+				w.Write(Challenge);
+			}
+		}
+	}
+
+	internal class A2S_RULES_Query : BaseChallengeQuery {
+		private const byte Header = 0x56;
+		public A2S_RULES_Query() : base(Header) { }
+		public A2S_RULES_Query(uint challenge) : base(Header, challenge) { }
+	}
+
+	public class A2S_RULES_Response : BaseResponse {
+
+		public Dictionary<string, string> Rules;
+
+		public A2S_RULES_Response(BinaryReader r) {
+			Rules = new Dictionary<string, string>();
+
+			ushort ruleCount = r.ReadUInt16();
+			for(ushort ruleIndex = 0; ruleIndex < ruleCount; ++ruleIndex) {
+				string key = r.ReadNullTerminatedUTF8String();
+				string value = r.ReadNullTerminatedUTF8String();
+				Rules.Add(key, value);
+			}
+		}
+
+		public bool TryGetCVar(string varName, out string varValue) => Rules.TryGetValue(varName, out varValue);
+		public string this[string varName] {
+			get { return Rules[varName]; }
+		}
+	}
+
+	internal class A2S_PLAYER_Query : BaseChallengeQuery {
+		private const byte Header = 0x55;
+		public A2S_PLAYER_Query() : base(Header) { }
+		public A2S_PLAYER_Query(uint challenge) : base(Header, challenge) { }
+	}
+
+	public class A2S_PLAYER_Response : BaseResponse {
+
+		public Player[] Players;
+
+		public A2S_PLAYER_Response(BinaryReader r) {
+			byte playerCount = r.ReadByte();
+
+			Players = new Player[playerCount];
+
+			for(byte playerIndex = 0; playerIndex < playerCount; ++playerIndex) {
+				byte index = r.ReadByte();
+				string name = r.ReadNullTerminatedUTF8String();
+				int score = r.ReadInt32();
+				float playtime = r.ReadSingle();
+
+				Players[playerIndex] = new Player(name, score, playtime);
+			}
+		}
+
+		public class Player {
+			public string Name;
+			public int Score;
+			public float Playtime;
+
+			public Player(string name, int score, float playtime) {
+				Name = name;
+				Score = score;
+				Playtime = playtime;
+			}
+
+			public override string ToString() {
+				return $"{Name} {Score}";
+			}
+		}
+	}
+
+	internal class A2S_SERVERQUERY_GETCHALLENGE_Response : BaseResponse {
+		public uint Challenge;
+		public A2S_SERVERQUERY_GETCHALLENGE_Response(BinaryReader r) {
+			Challenge = r.ReadUInt32();
+		}
+	}
+
+	internal abstract class BaseQuery {
+		abstract public void BuildQuery(Stream s);
+	}
+	public abstract class BaseResponse {
+	}
+}
