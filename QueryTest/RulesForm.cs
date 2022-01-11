038c951e8e0bd88368b9c3d6b80b0d67ea6311a1
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2022-01-11 20:23:59 +0100

    Factor out property generation to a separate method
    Also add more property types

--- QueryTest/RulesForm.cs
+++ QueryTest/RulesForm.cs
@@ -75,40 +75,67 @@ namespace QueryTest {
         public PropertyDescriptorCollection GetProperties(Attribute[] attributes) {
             var properties = new List<PropertyDescriptor>();
             foreach(var kv in rules) {
-				List<Attribute> atts = new List<Attribute>();
-				if(kv.Key.EndsWith("_version")) {
-					atts.Add(new CategoryAttribute("Version"));
-				} else if(kv.Key.StartsWith("tf_passtime")) {
-					atts.Add(new CategoryAttribute("Passtime"));
-				} else if(kv.Key.StartsWith("tf_arena_")) {
-					atts.Add(new CategoryAttribute("Arena"));
-				} else if(kv.Key.StartsWith("mp_tournament")) {
-					atts.Add(new CategoryAttribute("Tournament"));
-				} else if(kv.Key.StartsWith("tf_")) {
-					atts.Add(new CategoryAttribute("Team Fortress 2"));
-				} else if(kv.Key.StartsWith("sm_")) {
-					atts.Add(new CategoryAttribute("Source Mod"));
-				} else if(kv.Key.StartsWith("tv_")) {
-					atts.Add(new CategoryAttribute("Source TV"));
-				}
-
-				if(kv.Key=="sv_tags") {
-					properties.Add(new ListPropertyDescriptor(kv.Key, atts.ToArray(),','));
-					continue;
-				} else if(kv.Key=="mp_teamlist") {
-					properties.Add(new ListPropertyDescriptor(kv.Key, atts.ToArray(), ';'));
-					continue;
-				}
-
-
-				properties.Add(new RulePropertyDescriptor(kv.Key,atts.ToArray()));
-            }
+				properties.Add(descriptorForRule(kv.Key));
+			}
 
             return new PropertyDescriptorCollection(properties.ToArray());
         }
-    }
 
-	class RulePropertyDescriptor : PropertyDescriptor {
+		private static PropertyDescriptor descriptorForRule(string key) {
+			List<Attribute> atts = new List<Attribute>();
+			if(key.EndsWith("_version")) {
+				atts.Add(new CategoryAttribute("Version"));
+			} else if(key.StartsWith("tf_passtime")) {
+				atts.Add(new CategoryAttribute("Passtime"));
+			} else if(key.StartsWith("tf_arena_")) {
+				atts.Add(new CategoryAttribute("Arena"));
+			} else if(key.StartsWith("mp_tournament")) {
+				atts.Add(new CategoryAttribute("Tournament"));
+			} else if(key.StartsWith("tf_")) {
+				atts.Add(new CategoryAttribute("Team Fortress 2"));
+			} else if(key.StartsWith("sm_")) {
+				atts.Add(new CategoryAttribute("Source Mod"));
+			} else if(key.StartsWith("tv_")) {
+				atts.Add(new CategoryAttribute("Source TV"));
+			}
+
+			if(key == "sv_tags") {
+				return new ListPropertyDescriptor(key, atts.ToArray(), ',');
+			} else if(key == "mp_teamlist") {
+				return new ListPropertyDescriptor(key, atts.ToArray(), ';');
+			}
+
+			switch(key) {
+				case "sv_alltalk":
+				case "sv_cheats":
+				case "sv_password":
+				case "sv_pausable":
+				case "sv_voiceenable":
+				case "tf_arena_first_blood":
+				case "tf_arena_force_class":
+				case "tf_arena_use_queue":
+				case "tv_enable":
+				case "tv_password":
+				case "tf_allow_player_name_change":
+				case "tf_allow_player_use":
+				case "tf_gravetalk":
+				case "tf_halloween_allow_truce_during_boss_event":
+				case "tf_medieval":
+				case "tf_medieval_autorp":
+				case "tf_powerup_mode":
+				case "tf_server_identity_disable_quickplay":
+				case "tf_spells_enabled":
+				case "tf_use_fixed_weaponspreads":
+				case "tf_weapon_criticals":
+				case "tf_weapon_criticals_melee":
+					return new BoolPropertyDescriptor(key, atts.ToArray());
+			}
+
+			return new StringPropertyDescriptor(key, atts.ToArray());
+		}
+	}
+
+	abstract class RulePropertyDescriptor : PropertyDescriptor {
 		protected string key;
 
 		public RulePropertyDescriptor(string key, Attribute[] atts) : base(key, atts) {
@@ -119,14 +146,7 @@ namespace QueryTest {
 
 		public override bool IsReadOnly => true;
 
-		public override Type PropertyType => typeof(string);
-
 		public override bool CanResetValue(object component) => false;
-
-		public override object GetValue(object component) {
-			var dict = (Dictionary<string, string>)component;
-			return dict[key];
-		}
 
 		public override void ResetValue(object component) {
 			throw new NotImplementedException();
@@ -138,6 +158,54 @@ namespace QueryTest {
 		}
 
 		public override bool ShouldSerializeValue(object component) => false;
+	}
+
+	class StringPropertyDescriptor : RulePropertyDescriptor {
+		public StringPropertyDescriptor(string key, Attribute[] atts) : base(key, atts) {
+		}
+
+		public override Type PropertyType => typeof(string);
+
+		public override object GetValue(object component) {
+			var dict = (Dictionary<string, string>)component;
+			return dict[key];
+		}
+	}
+
+	class FloatPropertyDescriptor : RulePropertyDescriptor {
+		public FloatPropertyDescriptor(string key, Attribute[] atts) : base(key, atts) {
+		}
+
+		public override Type PropertyType => typeof(float);
+
+		public override object GetValue(object component) {
+			var dict = (Dictionary<string, string>)component;
+			return float.Parse(dict[key]);
+		}
+	}
+
+	class IntPropertyDescriptor : RulePropertyDescriptor {
+		public IntPropertyDescriptor(string key, Attribute[] atts) : base(key, atts) {
+		}
+
+		public override Type PropertyType => typeof(int);
+
+		public override object GetValue(object component) {
+			var dict = (Dictionary<string, string>)component;
+			return int.Parse(dict[key]);
+		}
+	}
+
+	class BoolPropertyDescriptor : RulePropertyDescriptor {
+		public BoolPropertyDescriptor(string key, Attribute[] atts) : base(key, atts) {
+		}
+
+		public override Type PropertyType => typeof(bool);
+
+		public override object GetValue(object component) {
+			var dict = (Dictionary<string, string>)component;
+			return int.Parse(dict[key])!=0;
+		}
 	}
 
 	class ListPropertyDescriptor : RulePropertyDescriptor {
